5d7339fd9ff3241b11e550c9d0e51e86fde67103
gurwnx222 <threader.corp@gmail.com>
2025-05-03 17:00:08 +0530

    Code optimisation and fixing file name inconsistencies

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import TNC from "./Pages/terms";
 import ImageSaver from "./Pages/ImageSaver";
+import VideoSaver from "./Pages/VideoSaver";
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/terms" element={<TNC />} />
             <Route path="/image" element={<ImageSaver />} />
+            <Route path="/video" element={<VideoSaver />} />
           </Routes>
         </Router>
       </div>
